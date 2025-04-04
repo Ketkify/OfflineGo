d23e9d71294d7767df28a07427f0edd837b31b79
Ketki Bansod <127389888+Ketkify@users.noreply.github.com>
2025-04-04 18:11:40 +0530

    HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,3 @@
-// HomeScreen.js (updated for better visual contrast)
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
