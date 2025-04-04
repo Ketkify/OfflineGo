9711436a6ffe6966d7fa6125dd3a3823dc538ad9
Ketki Bansod <127389888+Ketkify@users.noreply.github.com>
2025-04-04 18:06:22 +0530

    transactionRoutes.js

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -3,10 +3,8 @@ const router = express.Router();
 const { verifyToken } = require('../middleware/authMiddleware');
 const transactionController = require('../controller/transactionController');
 
-// 🔐 Get all transactions (protected)
 router.get('/', verifyToken, transactionController.getTransactions);
 
-// ➕ Add a new transaction (protected)
 router.post('/', verifyToken, transactionController.addTransaction);
 
 module.exports = router;
